a61f2a2a3d2cc6526bba9ccfd882b65ffda754b9
66lueflam144 <haerinwon@gmail.com>
2022-11-16 16:22:21 +0800

    update

---
+++ test2/fix.js
@@ -0,0 +1,25 @@
+<style>
+    #header {
+        background-color:black;
+        color:white;
+        text-align:center;
+        padding:5px;
+    }
+
+    #f1 {
+        line-height:30px;
+        background-color:#eeeee;
+        height:300px;
+        width:100px;
+        float:left;
+        padding:5px;
+    }
+
+    #footer {
+        background-color:black;
+        color:white;
+        clear:both;
+        text-align:center;
+        padding:5px;
+    }
+</style>
