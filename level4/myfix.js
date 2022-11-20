3937308cd729204e5f666275a5a7a6e22befb92f
66lueflam144 <haerinwon@gmail.com>
2022-11-20 23:00:47 +0800

    test

--- level4/myfix.js
+++ level4/myfix.js
@@ -1,3 +1,10 @@
-function myfix(){
-    document.getElementById("hidden").style.display="block";
+function hidetext(){
+    var mychar=document.getElementById("hidden");
+    mychar.style.display="none";
 }
+function showtext(){
+    var mychar=document.getElementById("hidden");
+mychar.style.display="block";
+}
+
+/*po*/
